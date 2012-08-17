03b025f2e17fe84654af25b28ae1f17dc03db43e
Marek Otahal <markotahal@gmail.com>
2012-08-17 03:09:10 +0200

    use relative path to java.exe on Windows, note PATH

--- pack-rootbeer.bat
+++ pack-rootbeer.bat
@@ -1,2 +1,2 @@
-
-"C:\Program Files\Java\jre6\bin\java.exe" -jar lib\pack.jar -mainjar dist\Rootbeer1.jar -directory lib -destjar Rootbeer.jar
+:: make sure your PATH points to java.exe
+java.exe -jar lib\pack.jar -mainjar dist\Rootbeer1.jar -directory lib -destjar Rootbeer.jar
